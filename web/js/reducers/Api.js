b302e7fab835955de490cf2813be58c7483d6a72
Jordi Coscolla <jordi@coscolla.net>
2016-04-09 21:42:40 +0200

    FIX loading was not being updated on the reduce

--- web/js/reducers/Api.js
+++ web/js/reducers/Api.js
@@ -8,10 +8,10 @@ let defaultState = {
 
 export default function(state = defaultState, action) {
   switch (action.type) {
-  case ActionTypes.COMIC_FETCHED:
+  case ActionTypes.COMIC_ON_LODING:
     return { ...state, loading: true}
   case ActionTypes.COMIC_SUCCEDED:
-    return { ...state, data: action.data}
+    return { ...state, data: action.data, loading: false}
   case ActionTypes.COMIC_FAILED:
     return { ...state, err: action.err}
   default:
