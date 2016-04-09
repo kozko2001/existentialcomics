af61207702d3ebd6d89b7e9df48f67bb983a565a
Jordi Coscolla <jordi@coscolla.net>
2016-04-09 07:41:30 +0200

    First steps... trying to understand everything

--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import 'whatwg-fetch';
 
 ReactDOM.render(<App />, document.getElementById('main'));
+
+fetch('http://comic.allocsoc.net/comics')
+    .then(function(response) {
+	return response.json();
+    }).then(function(json) {
+	console.log(json);
+    }).catch(function(err) {
+	console.log(err);
+    });
+
