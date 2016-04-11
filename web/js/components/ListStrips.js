9855f74dabf64034d6c02f94a482a22e2e4053df
Jordi Coscolla <jordi@coscolla.net>
2016-04-11 23:05:12 +0200

    Modify the strip react component to use the reducer state

--- web/js/components/ListStrips.js
+++ web/js/components/ListStrips.js
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
+import * as ListStripActions from '../actions/ListStripActions';
+import {bindActionCreators} from 'redux'
 
 class ListStrips extends Component {
 
   componentDidMount() {
-    console.log('mounted?');
+    const {dispatch} = this.props;
+    const {comicId} = this.props.params;
+    const actions = bindActionCreators(ListStripActions, dispatch);
+    actions.fetchStrips(comicId);
   }
   
   render() {
+    const {comicId} = this.props.params;
+    const strips = this.props.data[comicId];
     return (
 	<ul>
-	<li> {this.props.params.comicId} </li>
+	<li> {comicId} </li>
+	{
+	  strips.map((strip) =>
+		     <img src={'http://comic.allocsoc.net/comics/thumbnail/' + strip._id} />)
+	}
 	</ul>
     );
   }
 }
 
-export default ListStrips;
+export default connect((state) => state.listStripReducer)(ListStrips)
